d1b7bbf3b77263c2a4db30de8dbf2b950b47bf33
kalfung <14322@coderacademy.edu.au>
2023-06-30 23:56:25 +1000

    created user table columns

--- models/user.py
+++ models/user.py
@@ -5,4 +5,9 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    
+    email = db.Column(db.String, nullable=False, unique=True)
+    username = db.Column(db.String(30), nullable=False)
+    name =db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.Date, nullable=False)
