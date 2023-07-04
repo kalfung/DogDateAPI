55b604d1269ec1d2941c1764fcee2791da58a0b8
kalfung <14322@coderacademy.edu.au>
2023-07-04 15:05:39 +1000

    added POST_new_park route

--- blueprints/parks_bp.py
+++ blueprints/parks_bp.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from init import db
 from models.park import Park, ParkSchema
+from datetime import date
 
 parks_bp = Blueprint('parks', __name__, url_prefix='/parks')
 
@@ -19,4 +20,24 @@ def get_one_park(park_id):
     if park:
         return ParkSchema().dump(park)
     else:
-        return {'error': 'Card not found'}, 404
+        return {'error': 'Park not found'}, 404
+    
+# POST new park
+@parks_bp.route('/', methods=['POST'])
+def create_park():
+    # Load the incoming POST data via the schema
+    park_info = ParkSchema().load(request.json)
+    # Create a new Park instance from the park_info
+    park = Park(
+        name = park_info['name'],
+        latitude = park_info['latitude'],
+        longitude = park_info['longitude'],
+        date_registered = date.today(),
+        last_updated = date.today()
+    )
+
+    # Add and commit the new park to the session
+    db.session.add(park)
+    db.session.commit()
+    # Send the new park back to the client
+    return ParkSchema().dump(park), 201
