f7adc7ae25956c854f3eb1df976006ae53bebc29
kalfung <14322@coderacademy.edu.au>
2023-07-05 11:32:52 +1000

    created GET routes for dogs and events

--- blueprints/dogs_bp.py
+++ blueprints/dogs_bp.py
@@ -6,3 +6,22 @@ from flask_jwt_extended import jwt_required
 from blueprints.auth_bp import admin_required
 
 dogs_bp = Blueprint('dogs', __name__, url_prefix='/dogs')
+
+# GET all dogs - READ request
+@dogs_bp.route('/')
+@jwt_required()
+def all_dogs():
+    stmt = db.select(Dog).order_by(Dog.id) # could order_by name instead
+    dogs = db.session.scalars(stmt).all()
+    return DogSchema(many=True).dump(dogs)
+
+# GET one dog - READ request
+@dogs_bp.route('/<int:dog_id>')
+@jwt_required()
+def get_one_dog(dog_id):
+    stmt = db.select(Dog).filter_by(id=dog_id)
+    dog = db.session.scalar(stmt)
+    if dog:
+        return DogSchema().dump(dog)
+    else:
+        return {'error': 'Dog not found'}, 404
--- blueprints/events_bp.py
+++ blueprints/events_bp.py
@@ -6,3 +6,22 @@ from flask_jwt_extended import jwt_required
 from blueprints.auth_bp import admin_required
 
 events_bp = Blueprint('events', __name__, url_prefix='/events')
+
+# GET all events - READ request
+@events_bp.route('/')
+@jwt_required()
+def all_events():
+    stmt = db.select(Event).order_by(Event.id) # could order_by name instead
+    events = db.session.scalars(stmt).all()
+    return EventSchema(many=True).dump(events)
+
+# GET one event - READ request
+@events_bp.route('/<int:event_id>')
+@jwt_required()
+def get_one_event(event_id):
+    stmt = db.select(Event).filter_by(id=event_id)
+    event = db.session.scalar(stmt)
+    if event:
+        return EventSchema().dump(event)
+    else:
+        return {'error': 'Event not found'}, 404
